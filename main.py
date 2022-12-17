9d3767727e5d6347434d51d8d1b951155397d7e7
ino <87593133+inoUwU@users.noreply.github.com>
2022-12-17 22:31:34 +0900

    基本機能完成

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from ctypes import *
 from dotenv import load_dotenv
 import os
 import discord
+import openai
 
 # 環境変数の読み込み
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-OPENAI_APIKEY = os.getenv('OPENAI_APIKEY')
+openai.api_key = os.getenv('OPENAI_APIKEY')
 
 # bot command
-BOT_COMMAND = '!gpo'
+BOT_COMMAND = '!po'
 
 # 共有ライブラリの読み込み
 lib = cdll.LoadLibrary("./shared/ojosama.dll")
@@ -19,11 +20,31 @@ ojosama = lib.convertOjosama
 ojosama.argtypes = [c_char_p]
 ojosama.restype = c_char_p
 
-
+# bot 設定
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
+
+
+def fetch_answer(question: str):
+    prompt = f'''私は真実を答える賢い質問応答ボットです。 
+    Q: 日本の人口は？
+    A: 約1億人です。
+    Q: {question}？
+    A: '''
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+    print(response)
+    return response['choices'][0]['text']
 
 
 @client.event
@@ -37,8 +58,9 @@ async def on_message(message):
         return
 
     if message.content.startswith(BOT_COMMAND):
-        msg = message.content.replace(BOT_COMMAND, '').encode('utf-8')
-        msg = ojosama(msg).decode('utf-8')
-        await message.channel.send(msg)
+        question = message.content.replace(BOT_COMMAND, '').strip()
+        ans = fetch_answer(question=question)
+        res_msg = ojosama(ans.encode('utf-8')).decode('utf-8')
+        await message.channel.send(res_msg)
 
 client.run(DISCORD_TOKEN)
