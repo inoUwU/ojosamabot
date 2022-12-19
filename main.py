ed7253c2c1fccf2b19747d7d0b47d362ad369160
ino <87593133+inoUwU@users.noreply.github.com>
2022-12-19 22:39:37 +0900

    入力中の表示を行うように修正,事前学習を修正

--- main.py
+++ main.py
@@ -28,23 +28,55 @@ client = discord.Client(intents=intents)
 
 def fetch_answer(question: str):
     prompt = f'''私は真実を答える賢い質問応答ボットです。 
-    Q: 日本の人口は？
-    A: 約1億人です。
+    Q: バットマンとは？
+    A: バットマンは架空のコミック キャラクターです。
+    Q: トルサルプレクシティとは何ですか?
+    A: ?
+    Q: Devz9 とは何ですか?
+    A: ?
+    Q: ジョージ・ルーカスとは？
+    A: ジョージ・ルーカスは、スター・ウォーズで有名なアメリカの映画監督兼プロデューサーです。
+    Q: カリフォルニア州の州都は?
+    A: サクラメントです。
+    Q: 地球の周りを回っているのは?
+    A: 月です。
+    Q: フレッド・リッカーソンとは？
+    A: ?
+    Q: 原子とは何ですか? 
+    A: 原子は、すべてを構成する小さな粒子です。
+    Q: アルバン・ムンツとは?
+    A: ? 
+    Q: Kozar-09 とは何ですか?
+    A: ? 
+    Q: 火星にはいくつの月がありますか? 
+    A: フォボスとダイモスの 2 つです。
     Q: {question}？
     A: '''
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0,
-        max_tokens=300,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        stop=["\n"]
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0,
+            max_tokens=500,
+            top_p=1,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            stop=["\n"]
+        )
+        print(response)
+        return response['choices'][0]['text']
+    except Exception as e:
+        return e
+
+
+def fetch_image(image_content):
+    response = openai.Image.create(
+        prompt="a white siamese cat",
+        n=1,
+        size="1024x1024"
     )
-    print(response)
-    return response['choices'][0]['text']
+    image_url = response['data'][0]['url']
 
 
 @client.event
@@ -58,8 +90,10 @@ async def on_message(message):
         return
 
     if message.content.startswith(BOT_COMMAND):
+        await message.channel.typing()
         question = message.content.replace(BOT_COMMAND, '').strip()
         ans = fetch_answer(question=question)
+        print(ans)
         res_msg = ojosama(ans.encode('utf-8')).decode('utf-8')
         await message.channel.send(res_msg, reference=message)
 
